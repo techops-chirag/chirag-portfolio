1ccc4a62f48cf0f1c25a4501a5dffaa465807024
techops-chirag <chirag111saini@gmail.com>
2024-06-24 14:30:18 +0530

    sixth commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/techops-chirag",
   resume:
-    "https://drive.google.com/file/d/11AB92c7KrKUeby_wGv_Jfr9p7KVSWvBL/view?usp=sharing",
+    "https://drive.google.com/file/d/1pR9qLGHfm3QE10wzh9ck1c-yQo58oOTi/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/chirag-saini-602a88230/",
   twitter: "https://x.com/itzz_chirag",
   insta: "https://www.instagram.com/__chiraggs__/",
